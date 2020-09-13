ae4f69e59cd5db5b23d9a0028c04e7bf44004e59
viktoriazaharova <zaharova181114@gmail.com>
2020-09-13 13:37:17 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,75 @@
+$('.slider-masters').slick({
+    slideToShow: 4,
+    variableWidth: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    appendArrows: '.slider-masters__nav-wrapper',
+    responsive: [
+        {
+            breakpoint: 860,
+            settings: {
+                slidesToShow: 1,
+                variableWidth: false
+            }
+        }
+    ]
+});
+
+$(".slider-masters").on('afterChange', function (event, slick, currentSlide) {
+    $(".cp").text(currentSlide < 10 ? `${currentSlide + 1}` : currentSlide + 1);
+});
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
+$("body").on("click", ".btn-scroll-top", function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, "slow")
+});
+
+// Инициализация карты
+ymaps.ready(init);
+
+function init () {
+
+    //Центрирование и выбор масштаба карты
+    var myMap = new ymaps.Map('map', {
+        center: [55.758873, 37.779152],
+        zoom: 16
+    });
+
+    // Создание своей метки
+    var myPlacemark = new ymaps.Placemark(
+        // Координаты метки
+        [55.758873, 37.779152] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/loc.png',  // картинка иконки
+            iconImageSize: [46, 61],                                      // размеры картинки
+            iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    // Добавление метки на карту
+    myMap.geoObjects.add(myPlacemark);
+
+    //Элементы управления
+    myMap.controls
+    // Кнопка изменения масштаба
+        .add('zoomControl')
+        // Список типов карты
+        .add('typeSelector')
+        // Кнопка изменения масштаба - справа
+        .add('smallZoomControl', { right: 5, top: 75 })
+        // Стандартный набор кнопок
+        .add('mapTools')
+        //Линейка масштаба
+        .add(new ymaps.control.ScaleLine());
+}
